9955a7476d12b0d2eb40b36d9cd3daef2424359a
Adam McArthur <46480158+Sharpz7@users.noreply.github.com>
2023-08-17 10:48:49 -0600

    Update kubernetes.sh

--- script/kubernetes.sh
+++ script/kubernetes.sh
@@ -7,6 +7,6 @@ cd $GOPATH/src/k8s.io
 git clone https://github.com/Sharpz7/kubernetes.git
 cd $GOPATH/src/k8s.io/kubernetes
 
-sudo apt install build-essential net-tools jq
+sudo apt install -y build-essential net-tools jq
 pip3 install pyyaml && python3 -c "import yaml; print(yaml.__version__)"
 ./hack/install-etcd.sh

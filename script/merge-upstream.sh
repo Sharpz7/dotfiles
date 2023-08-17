9645a5b05968993b511a0784a814d9a6b44dd6fc
Adam McArthur <46480158+Sharpz7@users.noreply.github.com>
2023-08-17 09:46:27 -0600

    Update merge-upstream.sh

--- script/merge-upstream.sh
+++ script/merge-upstream.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-mkdir -p $GOPATH/src
-mkdir -p $GOPATH/src/k8s.io
-
-cd $GOPATH/src/k8s.io
-git clone https://github.com/Sharpz7/kubernetes.git
-cd $GOPATH/src/k8s.io/kubernetes
-
-./hack/install-etcd.sh
+git remote add upstream $REPO || true
+git fetch upstream
+git checkout master
+git merge upstream/master
+git push origin master

50bc19739c5cd4af51a3b243ad607da1f7cec901
Adam McArthur <46480158+Sharpz7@users.noreply.github.com>
2023-08-16 19:04:18 -0600

    Update kubernetes.sh

--- script/kubernetes.sh
+++ script/kubernetes.sh
@@ -1,5 +1,8 @@
 sudo apt install net-tools
 
+mkdir -p $GOPATH/src
+mkdir -p $GOPATH/src/k8s.io
+
 cd $GOPATH/src/k8s.io
 git clone https://github.com/Sharpz7/kubernetes.git
 cd $GOPATH/src/k8s.io/kubernetes
